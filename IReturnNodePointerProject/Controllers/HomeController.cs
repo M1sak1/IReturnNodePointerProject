8a3b385894fd67a5cc47ce2f5f9a429755d4148d
Joshua Corrigan <c3349863@uon.edu.au>
2023-07-31 13:34:51 +1000

    NugetPAckages
    Added the nuget packages to connect the database using Enitity framework core

--- IReturnNodePointerProject/Controllers/HomeController.cs
+++ IReturnNodePointerProject/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace IReturnNodePointerProject.Controllers
         public IActionResult Index()
         {
             ViewBag.Name = "Mary";
-            ViewBag.FV = 9999.99;
+            ViewBag.FV = 999999999.99;
             return View();
         }
     }
