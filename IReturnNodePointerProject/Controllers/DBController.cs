3baf4a657165079c4b6f5f221308ae611a34aa36
MrWaffaffles <joshcorrigan2@gmail.com>
2023-09-16 15:31:00 +1000

    Broken but healing

--- IReturnNodePointerProject/Controllers/DBController.cs
+++ IReturnNodePointerProject/Controllers/DBController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace IReturnNodePointerProject.Controllers
 {
-	public class DBController : Controller
-	{
-		public DBController() { }
-		public IActionResult Index()
-		{
-			return View();
-		}
-	}
+	public class DBController : IdentityDbContext<IdentityUser>
+    {
+        public DBController(DbContextOptions<DBController> options)
+       : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }  
+    }
 }
--- IReturnNodePointerProject/Program.cs
+++ IReturnNodePointerProject/Program.cs
@@ -1,61 +1,102 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-
-
-
-
-var builder = WebApplication.CreateBuilder(args);
-//enables session state and must be called before add controllerswithViews()
-
-builder.Services.AddMemoryCache();
-builder.Services.AddSession(options =>
+using IReturnNodePointerProject.Controllers;
+namespace IReturnNodePointerProject
 {
-    //options.IdleTimeout = TimeSpan.FromSeconds(60 * 5); //not currently used but the code to change the timeout feature on the session  (default is 20 min)
-    options.Cookie.HttpOnly = false; //would allow client side scripts to access cookies, security issue so false 
-    options.Cookie.IsEssential = true; //forces them to use cookies or not run the page, is essential for us as user permissions and stuff need to be set with an account 
-});
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //enables session state and must be called before add controllerswithViews()
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                //options.IdleTimeout = TimeSpan.FromSeconds(60 * 5); //not currently used but the code to change the timeout feature on the session  (default is 20 min)
+                options.Cookie.HttpOnly = false; //would allow client side scripts to access cookies, security issue so false 
+                options.Cookie.IsEssential = true; //forces them to use cookies or not run the page, is essential for us as user permissions and stuff need to be set with an account 
+            });
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
 
-builder.Services.AddDbContext<OnlineStoreContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineStoreContext")));
+            builder.Services.AddDbContext<OnlineStoreContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineStoreContext")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<OnlineStoreContext>();
+
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            //Before useendpoints() which is the mappcontrollerroute 
+            app.UseSession();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
 
-builder.Services.AddRazorPages();
+            app.UseRouting();
+            app.UseAuthentication(); ;
 
-var app = builder.Build();
+            app.UseAuthorization();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Account}/{action=Login}/{id?}");
+
+            app.MapControllerRoute(
+                name: "custom",
+                pattern: "{controller}/{action}"/*{activeConf}/div-{activeDiv}*/);
+            //Generic roles
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roles = new[] { "Admin", "Employee", "Customer" };
+
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+            //generic admin account 
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                string email = "admin@admin.com";
+                string password = "Password1!";
+                if (await userManager.FindByEmailAsync(email) == null)
+                {
+                    var user = new IdentityUser();
+                    user.Email = email;
+                    user.UserName = email;
+
+                    await userManager.CreateAsync(user, password);
+
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+                       
+               
+            }
+
+            app.Run();
+        }
+    }
 }
-//Before useendpoints() which is the mappcontrollerroute 
-app.UseSession();
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-
-app.UseRouting();
-app.UseAuthentication();;
-
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-	endpoints.MapRazorPages();
-});
-
-app.MapControllerRoute(    
-    name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
-
-app.MapControllerRoute(
-    name: "custom",
-    pattern: "{controller}/{action}"/*{activeConf}/div-{activeDiv}*/);
-
-app.Run();
