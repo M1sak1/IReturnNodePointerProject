ed7e70cce647715d3e4686e75e9f4dac7b3d3a4c
Joshua Corrigan <c3349863@uon.edu.au>
2023-10-09 13:56:19 +1100

    Login Is functional
    If only i had data to test it

--- IReturnNodePointerProject/Models/User.cs
+++ IReturnNodePointerProject/Models/User.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IReturnNodePointerProject.Models
 {
-    [NotMapped] //mvc believes as this class is using the IdentityUser stuff it has to be def.. might need a discriminated column
-    public class User : IdentityUser
+    
+    public class User 
     {
         [Key]
         public int? UserID { get; set; }
-        public string? userName { get; set; }
+        public string? UserName { get; set; }
         public string? email { get; set; }
         public string? Name { get; set; }
         public bool? IsAdmin { get; set; }
