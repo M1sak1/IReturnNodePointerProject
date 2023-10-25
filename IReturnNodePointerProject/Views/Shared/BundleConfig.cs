171088113188155feb47f57e9d656dc663954996
Andrew Cox <andrew.cox01110@gmail.com>
2023-10-25 12:41:18 +1100

    dfghjkl

--- IReturnNodePointerProject/Views/Shared/BundleConfig.cs
+++ IReturnNodePointerProject/Views/Shared/BundleConfig.cs
@@ -1,8 +1,19 @@
-﻿
+﻿using System.Web.Optimization;
+using System.Web;
 namespace IReturnNodePointerProject.Views.Shared
 {
 	public class BundleConfig
-	{	
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+				"~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+				"~/Scripts/bootstrap.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.css"));
+		}
 	}
 
 }
