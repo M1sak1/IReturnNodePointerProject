76d554724d515a4d70d72fb31513d8342a135a84
Andrew Cox <andrew.cox01110@gmail.com>
2023-10-25 13:35:53 +1100

    YEAH... FUCK YOU JQUERY. I WIN

--- IReturnNodePointerProject/Views/Shared/BundleConfig.cs
+++ IReturnNodePointerProject/Views/Shared/BundleConfig.cs
@@ -10,9 +10,6 @@ namespace IReturnNodePointerProject.Views.Shared
 				"~/Scripts/jquery-{version}.js"));
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 				"~/Scripts/bootstrap.js"));
-
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-				"~/Content/bootstrap.css"));
 		}
 	}
 
