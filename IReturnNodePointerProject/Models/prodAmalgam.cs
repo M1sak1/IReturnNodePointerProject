916ac118b81d065c0cf46039f52d9719fac56721
Andrew Cox <andrew.cox01110@gmail.com>
2023-10-29 16:13:41 +1100

    Product editing

--- IReturnNodePointerProject/Models/prodAmalgam.cs
+++ IReturnNodePointerProject/Models/prodAmalgam.cs
@@ -11,8 +11,10 @@ namespace IReturnNodePointerProject.Models
 		public string Author { get; set; }
 		public string Genre { get; set; }
 		public int GenreID { get; set; }
+		public int subGenreID { get; set; }
 		public string Stock { get; set; }
 		public int StokNum { get; set; }
+		public int SourceID { get; set; }
 		public int ProductID { get; set; }
 		//this is only used in the cart, but true to the classes name, it is an amalgam
 		public int Quantity { get; set; }
